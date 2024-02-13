3801797a21497af56b1f1e3facf903982a85ddbe
manseerat21 <smanseeratsinghgill21@gmail.com>
2024-02-13 05:35:06 -0600

    new file:   app/(main)/(routes)/home/page.tsx 	new file:   app/(main)/_components/naviagtion.tsx 	new file:   app/(main)/_components/user-item.tsx 	new file:   app/(main)/layout.tsx 	modified:   app/(marketing)/_components/heading.tsx 	modified:   app/(marketing)/_components/navbar.tsx 	modified:   app/layout.tsx 	new file:   components/providers/convex-provider.tsx 	new file:   components/spinner.tsx 	new file:   components/ui/avatar.tsx 	modified:   components/ui/dropdown-menu.tsx 	new file:   convex/README.md 	new file:   convex/_generated/api.d.ts 	new file:   convex/_generated/api.js 	new file:   convex/_generated/dataModel.d.ts 	new file:   convex/_generated/server.d.ts 	new file:   convex/_generated/server.js 	new file:   convex/auth.config.js 	new file:   convex/tsconfig.json 	modified:   package-lock.json 	modified:   package.json 	new file:   public/empty-dark.png 	new file:   public/empty.png

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,9 +1,15 @@
 "use client";
 
+import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
+import { SignInButton } from "@clerk/clerk-react";
+import Link from "next/link";
 
 export const Heading = () => {
+    const { isAuthenticated, isLoading } = useConvexAuth();
+
     return ( 
         <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -13,10 +19,27 @@ export const Heading = () => {
                 NotionBud is the connected workspace where <br />
                 better, faster work happens.
             </h3>
-            <Button>
-                Enter NotionBud
+            {isLoading && (
+                <div className="w-full flex items-center justify-center">
+                    <Spinner size="lg" />
+                </div>
+            )}
+            {isAuthenticated && !isLoading && (
+                <Button asChild>
+                    <Link href="/home">
+                    Enter NotionBud
                 <ArrowRight className="h-4 w-4 ml-2" />
-            </Button>
+                    </Link>
+                </Button>
+            )}
+            {(!isAuthenticated && !isLoading && (
+                <SignInButton mode="modal">
+                    <Button>
+                        Get NotionBud free
+                        <ArrowRight className="h-4 w-4 ml-2" />
+                    </Button>
+                </SignInButton>
+            ))}
         </div>
      );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
+import { Toaster } from "sonner";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +20,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="CLASS" defaultTheme="system" enableSystem disableTransitionOnChange storageKey="notion_bud-theme-2">
-        {children}
-        </ThemeProvider>
+        <ConvexClientProvider>
+          <ThemeProvider attribute="CLASS" defaultTheme="system" enableSystem disableTransitionOnChange storageKey="notion_bud-theme-2">
+            <Toaster position="bottom-center" />
+          {children}
+          </ThemeProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   );
