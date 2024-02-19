453fe9a8e32234efc8e52ba7b20eb4e2ff11528d
manseerat21 <smanseeratsinghgill21@gmail.com>
2024-02-18 22:44:49 -0600

    modified:   app/(main)/_components/naviagtion.tsx 	new file:   app/(main)/_components/trash-box.tsx 	new file:   components/modals/confirm-modal.tsx 	new file:   components/search-command.tsx 	new file:   components/ui/alert-dialog.tsx 	new file:   components/ui/command.tsx 	new file:   components/ui/dialog.tsx 	new file:   components/ui/input.tsx 	new file:   components/ui/popover.tsx 	modified:   components/ui/skeleton.tsx 	modified:   convex/documents.ts 	new file:   hooks/use-search.tsx 	modified:   package-lock.json 	modified:   package.json

---
+++ components/modals/confirm-modal.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogTitle, AlertDialogTrigger } from "@radix-ui/react-alert-dialog";
+import { AlertDialogFooter, AlertDialogHeader } from "../ui/alert-dialog";
+
+interface ConfirmModalProps {
+    children: React.ReactNode;
+    onConfirm: () => void;
+};
+
+export const ConfirmModal = ({
+    children,
+    onConfirm
+}: ConfirmModalProps) => {
+    const handleConfirm = (
+        e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    ) => {
+        e.stopPropagation();
+        onConfirm();
+    }
+
+
+    return (
+        <AlertDialog>
+            <AlertDialogTrigger onClick={(e) => e.stopPropagation()} asChild>
+                {children}
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>
+                        Are you absolutely sure?
+                    </AlertDialogTitle>
+                    <AlertDialogDescription>
+                        This action can't be undone.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel onClick={e => e.stopPropagation()}>
+                        Cancel
+                    </AlertDialogCancel>
+                    <AlertDialogAction onClick={handleConfirm}>
+                        Confirm
+                    </AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    )
+}
